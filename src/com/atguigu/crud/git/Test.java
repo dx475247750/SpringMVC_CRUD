69ad9ad445e6a3394c2927f2199cbe8220522c24
dx475247750 <18334726627@163.com>
2016-05-27 23:13:06 +0800

    Update Test.java

--- src/com/atguigu/crud/git/Test.java
+++ src/com/atguigu/crud/git/Test.java
@@ -3,5 +3,7 @@ package com.atguigu.crud.git;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("111111111111111");
+	System.out.println("WWWWWWWWWWWWWWWW");
+
 }
 }
